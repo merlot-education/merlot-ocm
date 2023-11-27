b72698ccf1ef768dd366dfbb0ea7dad00d5b275f
Berend Sliedrecht <berend@animo.id>
2023-11-27 13:26:53 +0100

    feat(ssi): use the new event structure in the ssi-abstraction
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- apps/ssi-abstraction/src/agent/connections/connections.controller.ts
+++ apps/ssi-abstraction/src/agent/connections/connections.controller.ts
@@ -1,5 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
+import { EventDidcommConnectionsGetAll } from '@ocm/shared';
 
 import { ConnectionsService } from './connections.service.js';
 
@@ -8,7 +9,9 @@ export class ConnectionsController {
   public constructor(private connectionsService: ConnectionsService) {}
 
   @MessagePattern('didcomm.connections.getAll')
-  public async getAll() {
-    return await this.connectionsService.getAll();
+  public async getAll(): Promise<EventDidcommConnectionsGetAll> {
+    return new EventDidcommConnectionsGetAll({
+      connections: await this.connectionsService.getAll(),
+    });
   }
 }
